5dd30c6244e0bd1290cb466a118d7b5f92e1437e
PK <nozumaz@gmail.com>
2023-05-18 10:26:49 -0400

    update NavBar, Home, and About

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>not much to say</p>
+      <img src ={image}/>
+    </div>
+  );
 }
 
 export default About;
